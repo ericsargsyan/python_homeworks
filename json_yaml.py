db02817bea35f8df26be6b94b68c78fc8007e13d
Eric <eric.sargsiann@gmail.com>
2021-01-22 00:49:50 +0400

    js yml

---
+++ json_yaml.py
@@ -0,0 +1,19 @@
+import os
+import json
+import yaml
+path = os.path.join(os.getcwd(),"sample_json.json")
+with open(path,"r") as file_json:
+    data = json.load(file_json)
+final_list = []
+# print(data["items"])
+for dict_ in data['items']:
+    new_data = {}
+    if dict_['age'] > 36:
+        new_data['name'] = dict_['name']
+        new_data["age"] = dict_["age"]
+        final_list.append(new_data)
+result = {"items":final_list}  
+print(result)
+with open("new_yaml.yml", "w") as file:
+    yaml.dump(result, file, indent=4)
+# print(type(data))
